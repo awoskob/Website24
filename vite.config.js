d6732e80bc0ff224b32b34d800a924c9291e118a
awoskob <awoskob@gmail.com>
2024-10-25 16:33:15 -0400

    jpg asset

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import inject from "@rollup/plugin-inject";
 
 export default {
     base: '/Website24/',
-    assetsInclude: ['**/*.stl', '**/*.obj', '**/*.mtl'],
+    assetsInclude: ['**/*.stl', '**/*.obj', '**/*.mtl',  '**/*.jpg'],
     plugins: [
         inject({   // => that should be first under plugins array
             $: 'jquery',
